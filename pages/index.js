19e25cbe452ba57f14527adf0558c792a8fa287a
Martin Glynn <oline@bestweb.net>
2024-08-02 17:13:10 -0400

    should be live

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,34 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>CFRgpt.com AI for Healthcare Compliance</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <style jsx>{`
+        li {
+          list-style-type: none;
+          margin: 0.4em 0;
+        }
+      `}</style>
 
       <main>
-        <Header title="Welcome to my app and love this stuff" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <a href="/about.html">About</a>
-        <a href="/patience.html">Home</a>
+        <Header title="CFRgpt.com" />
+        <p className="description">AI for Healthcare Compliance</p>
+        <ul class="home">
+          <li>
+            <a href="/usecases.html">AI Use Cases in Healthcare Compliance</a>
+          </li>
+          <li>
+            <a href="/requirements.html">Requirements and Implementation</a>
+          </li>
+
+          <li>
+            <a href="/examples.html">Practical Examples</a>
+          </li>
+          <li>
+            <a href="/about.html">About Us</a>
+          </li>
+        </ul>
       </main>
 
       <Footer />
